410ff85deee750b8ae4b6144b1f9739ba599d01c
Jean <gitslagga@gmail.com>
2019-06-03 16:37:03 +0800

    add thinkinjava file

---
+++ unit/src/main/java/top/slagga/Deamons.java
@@ -0,0 +1,50 @@
+package top.slagga;
+
+import java.io.*;
+
+class Daemon extends Thread {
+	private static final int SIZE = 10;
+	private Thread[] t = new Thread[SIZE];
+	public Daemon() {
+		setDaemon(true);
+		start();
+	}
+	
+	public void run() {
+		for(int i = 0; i < SIZE; i++)
+			t[i] = new DaemonSpawn(i);
+		for(int i = 0; i < SIZE; i++)
+			System.out.println("t[" + i + "].isDaemon() = "
+					+ t[i].isDaemon());
+		while(true)
+			yield();
+	}
+}
+
+class DaemonSpawn extends Thread {
+	public DaemonSpawn(int i) {
+		System.out.println("DarmonSpawn " + i + " started");
+		start();
+	}
+	
+	public void run() {
+		while(true)
+			yield();
+	}
+}
+
+public class Deamons {
+	public static void main(String[] args) {
+		Thread d = new Daemon();
+		System.out.println("d.isDaemon() = " + d.isDaemon());
+		
+		BufferedReader stdin = 
+				new BufferedReader(
+						new InputStreamReader(System.in));
+		System.out.println("Waiting for CR");
+		try {
+			stdin.readLine();
+//			Thread.sleep(0);	// how long does the thread stop	
+		} catch (Exception e) {}
+	}
+}
